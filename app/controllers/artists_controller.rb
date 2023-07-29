6bcb3d7c63a6599536e2c08c0a8ff41e70851048
Evgeny Varnakov <evgeniy.varnakov@sbermarket.ru>
2023-07-29 18:02:34 +0300

    solution: day 1 - task 3

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -11,6 +11,17 @@ class ArtistsController < ApplicationController
     end
   end
 
+  def tracks
+    artist = Artist.find(params[:id])
+    page = params[:page].to_i
+    tracks = artist.tracks.popularity_ordered.offset(page * 10).limit(10)
+    if turbo_frame_request?
+      render partial: "tracks", locals: {artist:, tracks:, page:}
+    else
+      render locals: {artist:, tracks:}
+    end
+  end
+
   private
 
   def selected_albums(albums, album_type)

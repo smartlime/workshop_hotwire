083adce7e60a0e88650a029c7daf3219a6669911
Evgeny Varnakov <evgeniy.varnakov@sbermarket.ru>
2023-07-30 12:45:34 +0300

    solution: day 2 - task 2

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,6 @@ window.Stimulus = application;
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
 eagerLoadControllersFrom("controllers", application);
+
+import PasswordVisibility from "stimulus-password-visibility";
+application.register('password-visibility', PasswordVisibility)
